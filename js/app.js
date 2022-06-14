c5c7b36cfaec5dbc6236b7509c7a29c63f29ed63
TihimVecherom <98401044+TihimVecherom@users.noreply.github.com>
2022-06-14 16:55:15 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4047,6 +4047,19 @@
             }
         }
     };
+    $(document).ready((function() {
+        $("a").on("click", (function(event) {
+            if ("" !== this.hash) {
+                event.preventDefault();
+                var hash = this.hash;
+                $("html, body").animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, (function() {
+                    window.location.hash = hash;
+                }));
+            }
+        }));
+    }));
     window["FLS"] = true;
     isWebp();
     spollers();
